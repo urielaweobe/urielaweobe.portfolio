8697bada50ce119ac30660299793680e9cc6e4e2
urielaweobe <urielaweobe@gmail.com>
2025-10-17 11:14:07 +0100

    feat: update font and restructure portfolio routes with new pages

---
+++ app/portfolio/about.tsx
@@ -0,0 +1,17 @@
+export function meta() {
+  return [
+    { title: "Uriel Awe-Obe" },
+    {
+      name: "description",
+      content: "Learn more about Uriel Awe-Obe's journey and experience.",
+    },
+  ];
+}
+
+export default function About() {
+  return (
+    <div>
+      <h2>This is the about page for Uriel Awe-Obe</h2>
+    </div>
+  );
+}
