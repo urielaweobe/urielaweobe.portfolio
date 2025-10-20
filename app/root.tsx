4dfcd16fcf4df83780073f217513e1441847a57c
urielaweobe <urielaweobe@gmail.com>
2025-10-21 00:22:25 +0100

    feat: integrate SpeedInsights for enhanced performance monitoring

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import {
   isRouteErrorResponse,
   Links,
@@ -34,6 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
 				<Analytics />
+        <SpeedInsights />
         {children}
         <ScrollRestoration />
         <Scripts />
